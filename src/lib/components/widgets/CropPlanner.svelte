71e4ef817b0f26e943c2aaa621a37f64fa48e773
Monkie <a_brine@yahoo.com>
2023-06-26 18:22:26 +0100

    more work done on "crop planner" widget

--- src/lib/components/widgets/CropPlanner.svelte
+++ src/lib/components/widgets/CropPlanner.svelte
@@ -1,88 +1,281 @@
 <script lang="ts">
-	import { server_loads } from './../../../../.svelte-kit/generated/client/app.js';
+	import { fade, fly } from 'svelte/transition';
 	import WeedGrowing from '$lib/data/WeedGrowingStats.json';
-	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+	import { Tab, TabGroup } from '@skeletonlabs/skeleton';
 	let selectedSeed = WeedGrowing[0];
+	$: {
+		if (selectedSeed.inOrOut.toLowerCase() === 'outdoor') {
+			if (tabSet === 2) tabSet = 0;
+			showSetUpCost = false;
+		} else {
+			showSetUpCost = true;
+		}
+	}
 	let cropAmount = 1;
+	let showSetUpCost: boolean;
+	let tabSet: number = 0;
+	let sellPrice: number = 10;
 </script>
 
-<div class="h-full my-10 flex flex-col gap-2">
-	<label class="label">
-		<span>Strain</span>
-		<select placeholder={'select'} bind:value={selectedSeed} class="select">
-			{#each WeedGrowing as seed}
-				<option value={seed}>{seed.strain}</option>
-			{/each}
-		</select>
-	</label>
+<div class="flex items-center w-full h-full justify-center">
+	<div class="flex gap-2 justify-center items-center w-full h-full overflow-scroll flex-wrap">
+		<label class="label w-48">
+			<span>Strain</span>
+			<select placeholder={'select'} bind:value={selectedSeed} class="select">
+				{#each WeedGrowing as seed}
+					<option value={seed}>{seed.strain}</option>
+				{/each}
+			</select>
+		</label>
 
-	<label class="label">
-		<span>Crop Size</span>
-		<input min="1" bind:value={cropAmount} class="input" title="seed_amount" type="number" />
-	</label>
-	<label class="label">
-		<span>Seed Cost</span>
-		<input value={`$${selectedSeed.price}`} readonly class="input" title="seed_cost" type="text" />
-	</label>
-	<label class="label">
-		<span>Crop Cost</span>
-		<input
-			value={`$${selectedSeed.price * cropAmount}`}
-			readonly
-			class="input"
-			title="seed_cost"
-			type="text"
-		/>
-	</label>
-	<label class="label">
-		<span>Grown</span>
-		<input value={selectedSeed.inOrOut} readonly class="input" title="seed_cost" type="text" />
-	</label>
-	<label class="label">
-		<span>Grow Time</span>
-		<input
-			value={selectedSeed.hours + ' Hrs'}
-			readonly
-			class="input"
-			title="seed_cost"
-			type="text"
-		/>
-	</label>
-	<label class="label">
-		<span>Water Every </span>
-		<input
-			value={selectedSeed.waterH + ' Hrs'}
-			readonly
-			class="input"
-			title="seed_cost"
-			type="text"
-		/>
-	</label>
-	<label class="label">
-		<span>Needs Watering</span>
-		<input
-			value={(selectedSeed.hours / selectedSeed.waterH).toFixed(1) + ' Times'}
-			readonly
-			class="input"
-			title="seed_cost"
-			type="text"
-		/>
-	</label>
-	<Accordion class={'card mb-4 variant-ghost-secondary'}>
-		<AccordionItem>
-			<svelte:fragment slot="lead">
-				<!-- <i class={`fa-solid fa-info-circle`} /> -->
+		<label class="label w-48">
+			<span>Crop Size</span>
+			<input min="1" bind:value={cropAmount} class="input" title="seed_amount" type="number" />
+		</label>
+		<label class="label w-48">
+			<span>Seed Cost</span>
+			<input
+				value={`$${selectedSeed.price}`}
+				readonly
+				class="input"
+				title="seed_cost"
+				type="text"
+			/>
+		</label>
+		<label class="label w-48">
+			<span>Crop Cost</span>
+			<input
+				value={`$${selectedSeed.price * cropAmount}`}
+				readonly
+				class="input"
+				title="seed_cost"
+				type="text"
+			/>
+		</label>
+		<label class="label w-48">
+			<span>Grown</span>
+			<input value={selectedSeed.inOrOut} readonly class="input" title="seed_cost" type="text" />
+		</label>
+		<label class="label w-48">
+			<span>Grow Time</span>
+			<input
+				value={selectedSeed.hours + ' Hrs'}
+				readonly
+				class="input"
+				title="seed_cost"
+				type="text"
+			/>
+		</label>
+		<label class="label w-48">
+			<span>Water Every </span>
+			<input
+				value={selectedSeed.waterH + ' Hrs'}
+				readonly
+				class="input"
+				title="seed_cost"
+				type="text"
+			/>
+		</label>
+		<label class="label w-48">
+			<span>Needs Watering</span>
+			<input
+				value={(selectedSeed.hours / selectedSeed.waterH).toFixed(1) + ' Times'}
+				readonly
+				class="input"
+				title="seed_cost"
+				type="text"
+			/>
+		</label>
+		<TabGroup class={'w-full px-5 py-3'} justify="justify-center">
+			<Tab bind:group={tabSet} name="tab1" value={0}>
+				<span>Yield</span>
+			</Tab>
+			<Tab bind:group={tabSet} name="tab2" value={1}>Sell</Tab>
+			{#if showSetUpCost}
+				<Tab bind:group={tabSet} name="tab3" value={2}>Set Up</Tab>
+			{/if}
+
+			<!-- Tab Panels --->
+			<svelte:fragment slot="panel">
+				{#if tabSet === 0}
+					<div class="flex truncate flex-wrap gap-4 justify-center">
+						<!-- Native Table Element -->
+						<table class="table table-hover max-w-sm table-compact">
+							<thead>
+								<tr>
+									<th>Per Plant</th>
+									<th>G</th>
+									<th>G/H</th>
+									<th>%</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Wet</td>
+									<td>205</td>
+									<td>2.62</td>
+									<td />
+								</tr>
+								<tr>
+									<td>Dry</td>
+									<td>74.10</td>
+									<td>0.95</td>
+									<td>36.00%</td>
+								</tr>
+								<tr>
+									<td>Loss</td>
+									<td>130.90</td>
+									<td />
+									<td>64.00%</td>
+								</tr>
+							</tbody>
+						</table>
+						<table class="table table-hover max-w-sm table-compact">
+							<thead>
+								<tr>
+									<th>Crop</th>
+									<th>G</th>
+									<th>G/H</th>
+									<th>%</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Wet</td>
+									<td>1640</td>
+									<td>20.96</td>
+									<td />
+								</tr>
+								<tr>
+									<td>Dry</td>
+									<td>593</td>
+									<td>7.60</td>
+									<td>36.00%</td>
+								</tr>
+								<tr>
+									<td>Loss</td>
+									<td>1047.2</td>
+									<td />
+									<td>64.00%</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				{:else if tabSet === 1}
+					<div class="flex truncate flex-wrap gap-4 justify-center">
+						<!-- Native Table Element -->
+						<table class="table table-hover max-w-sm table-compact">
+							<thead>
+								<tr>
+									<th>LSD Gang</th>
+									<th>Per G</th>
+									<th>Total</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Cost</td>
+									<td>$0.61</td>
+									<td>$360.00</td>
+								</tr>
+								<tr>
+									<td>Sell</td>
+									<td>$4.00</td>
+									<td>$2,371.20</td>
+								</tr>
+								<tr>
+									<td>Profit</td>
+									<td>$3.39</td>
+									<td>$2,011.20</td>
+								</tr>
+							</tbody>
+						</table>
+						<table class="table table-hover max-w-sm table-compact">
+							<thead>
+								<tr>
+									<th>Client</th>
+									<th>Per G</th>
+									<th>Total</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Cost</td>
+									<td>$0.61</td>
+									<td>$360.00</td>
+								</tr>
+								<tr>
+									<td>Sell</td>
+									<td>
+										<div
+											class="input items-center p-1 justify-center flex rounded-container-token w-20"
+										>
+											$
+											<input
+												min="10"
+												bind:value={sellPrice}
+												class="input border-none p-0 m-0 rounded-container-token"
+												title="sell_price"
+												type="number"
+											/>
+										</div>
+									</td>
+									<td>$2,371.20</td>
+								</tr>
+								<tr>
+									<td>Profit</td>
+									<td>$3.39</td>
+									<td>$2,011.20</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				{:else if tabSet === 2}
+					<div class="flex truncate flex-wrap gap-4 justify-center">
+						<!-- Native Table Element -->
+						<table class="table table-hover max-w-sm table-compact">
+							<thead>
+								<tr>
+									<th>Equipment</th>
+									<th>#</th>
+									<th>Each</th>
+									<th>Total</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Lights</td>
+									<td>2</td>
+									<td>$300</td>
+									<td>$685.71</td>
+								</tr>
+								<tr>
+									<td>Pots</td>
+									<td>2</td>
+									<td>$300</td>
+									<td>$685.71</td>
+								</tr>
+								<tr>
+									<td>Dryers</td>
+									<td>2</td>
+									<td>$300</td>
+									<td>$685.71</td>
+								</tr>
+							</tbody>
+							<tfoot>
+								<tr class="table-row-checked">
+									<th class="text-right" colspan="3">Setup Cost</th>
+									<td>100</td>
+								</tr>
+								<tr class="table-row-checked">
+									<th class="text-right" colspan="3">Total Crop Cost</th>
+									<td>100</td>
+								</tr>
+							</tfoot>
+						</table>
+					</div>
+				{/if}
 			</svelte:fragment>
-			<svelte:fragment slot="summary"
-				><span class="font-semibold">Set Up Costs</span></svelte:fragment
-			>
-			<svelte:fragment slot="content">
-				<span
-					>Just select a widget to add it to your mix board then arrange them in any order or layout
-					by just dragging them around the mix board.</span
-				>
-			</svelte:fragment>
-		</AccordionItem>
-		<!-- ... -->
-	</Accordion>
+		</TabGroup>
+	</div>
 </div>
