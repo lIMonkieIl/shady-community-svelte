c2f94f63befd3ef4c55aa40b27107f05337f1d24
Monkie <a_brine@yahoo.com>
2023-06-26 10:27:15 +0100

    c

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import * as child_process from 'node:child_process';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +11,10 @@ const config = {
 		inspector: true
 	},
 	kit: {
+		version: {
+			name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+			pollInterval: 10000
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
